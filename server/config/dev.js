ec323ba45f7523a40c7a22cce5e5f97edc40cdc4
Trevor Meadows <tlm04070@gmail.com>
2018-02-15 13:51:28 -0500

    initial

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "1078528340270-eed9del8t8e1jmasv4tkh9otkdug7aml.apps.googleusercontent.com",
+  googleClientSecret: "9tbUBX-KgivVqNTeYXyhMvv1",
+  mongoURI: "mongodb://user:password@ds239128.mlab.com:39128/detail-devil-dev",
+  cookieKey: "randomstring"
+};
